5cf4bffd5d2871e6079fbf276b7cc2b639577599
sudoQ <vorga88@gmail.com>
2018-07-05 15:37:25 +0300

    add html for timer with flex

--- timer.js
+++ timer.js
@@ -1,1 +1,95 @@
-var timerDiv = document.querySelector(".timer"); var time = 60; var timerStart = document.querySelector(".timer__start"); timerStart.addEventListener('click', start); timerDiv.innerHTML = "<span> 01:00 </span>"; function start() { var timerId = setInterval(function () { var minutes = Math.floor(time / 60); var seconds = time % 60; minutes = ("0" + minutes).slice(-2); seconds = ("0" + seconds).slice(-2); timerDiv.innerHTML = "<span>" + minutes + ":" + seconds + "</span>"; if (time == 0) { clearInterval(timerId); } --time; }, 1000); }
+var PATH_TO_BUZZER = "";
+
+function Timer() {
+    var time;
+    var timerId;
+    var MAX = 3600;
+    var MIN = 0;
+    var defaultTime = 60;
+    var timerDiv = document.querySelector(".timer__counter");
+    var timerStart = document.querySelector(".timer__start");
+    var plus5min = document.querySelector(".timer__5mins .top-trangle");
+    var minus5min = document.querySelector(".timer__5mins .bottom-trangle");
+
+    this.init = function () {
+        setDefaultTime();
+        timerStart.addEventListener('click', startHandler);
+    }
+
+    function getMinutes() {
+        var minutes = Math.floor(time / 60); //check if there is one seond more then should be at start
+        return ("0" + minutes).slice(-2);
+    }
+
+    function getSeconds() {
+        var seconds = time % 60;
+        return ("0" + seconds).slice(-2);
+    }
+
+    function updateCounter() {
+        timerDiv.innerHTML = "" + getMinutes() + ":" + getSeconds() + "";
+    }
+
+    function setDefaultTime() {
+        time = defaultTime;
+        updateCounter();
+    }
+
+    function startHandler() {
+        if (timerStart.innerText == "Pause") {
+            timerStart.innerText = "Start";
+            clearInterval(timerId);
+        }
+        else {
+            timerStart.innerText = "Pause";
+            startTimer();
+        }
+    }
+
+    function startTimer() {
+        timerId = setInterval(function () {
+
+            --time;
+            updateCounter();
+
+            if (time == MIN) {startHandler(); }
+
+
+        }, 1000);
+    }
+
+    function updateTime(seconds) {
+        if (time + seconds > MAX) {
+            time = 3600;
+        }
+        else if (time + seconds < MIN) {
+            time = MIN;
+        }
+        else {
+            time = time + seconds;
+        }
+    }
+    function plus5mins() {
+        updateTime(300);
+    }
+    function minus10mins() {
+        updateTime(-300);
+    }
+    function plus1min() {
+        updateTime(60);
+    }
+    function minus1min() {
+        updateTime(-60);
+    }
+    function plus10secs() {
+        updateTime(10);
+    }
+    function minus10secs() {
+        updateTime(-10);
+    }
+}
+
+
+var newTimer = new Timer();
+
+newTimer.init();
